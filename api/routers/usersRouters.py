facc56b13ff8f50fac907cad26c00a5de59a3297
Ashley Phan <ashleyphan17@gmail.com>
2023-08-25 19:36:52 +0000

    Login front end functionality #17

--- api/routers/usersRouters.py
+++ api/routers/usersRouters.py
@@ -71,6 +71,19 @@ def delete_user(
     return True
 
 
+@router.get("/token", response_model=UserToken | None)
+async def get_token(
+    request: Request,
+    account: UserOut = Depends(authenticator.try_get_current_account_data)
+) -> UserToken| None:
+    if account and authenticator.cookie_name in request.cookies:
+        return {
+            "access_token": request.cookies[authenticator.cookie_name],
+            "type": "Bearer",
+            "user": account,
+        }
+
+
 @router.post("/api/users", response_model=UserToken | HttpError)
 async def create_account(
     info: UserIn,
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,6 +3,10 @@ import Construct from "./Construct.js";
 import ErrorNotification from "./ErrorNotification";
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./LoginForm.js";
+import { AuthProvider } from "@galvanize-inc/jwtdown-for-react";
+
 function App() {
   const [launchInfo, setLaunchInfo] = useState([]);
   const [error, setError] = useState(null);
@@ -10,16 +14,12 @@ function App() {
   useEffect(() => {
     async function getData() {
       let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
       let response = await fetch(url);
-      console.log("------- hello? -------");
       let data = await response.json();
 
       if (response.ok) {
-        console.log("got launch data!");
         setLaunchInfo(data.launch_details);
       } else {
-        console.log("drat! something happened");
         setError(data.message);
       }
     }
@@ -30,6 +30,14 @@ function App() {
     <div>
       <ErrorNotification error={error} />
       <Construct info={launchInfo} />
+      <BrowserRouter>
+        <AuthProvider baseUrl="http://localhost:8000">
+          <Routes>
+            <Route path="login" element={<LoginForm/>}></Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+
     </div>
   );
 }
