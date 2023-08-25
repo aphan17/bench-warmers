c52543110a288074fb2aa8a1ea2b2c179b62771a
Corben M <corbenm99@gmail.com>
2023-08-24 17:15:49 -0700

    in progress

--- ghi/src/UserProfilePage.js
+++ ghi/src/UserProfilePage.js
@@ -1,7 +1,14 @@
 import React, {useState} from "react";
 
+
 function UserProfilePage() {
-    // const [user, setUser] = useState({});
+    const [user, setUser] = useState([])
+
+    async function getUser() {
+        const response = await fetch('')
+    }
+
+
     console.log("is this page alive?")
     return (
         <p>Help</p>
