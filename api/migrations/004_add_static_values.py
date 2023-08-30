fc13f72cced0dd0edb688ba0c144e99c45baabb2
Corben M <corbenm99@gmail.com>
2023-08-30 11:45:54 -0700

    Added a few more gyms

--- api/migrations/004_add_static_values.py
+++ api/migrations/004_add_static_values.py
@@ -3,8 +3,11 @@ steps = [
         """
             INSERT INTO locations (gym) VALUES
             ('Planet Fitness'),
-            ('Sunset Gym'),
-            ('24hr fitness');
+            ('24hr fitness'),
+            ('Golds'),
+            ('Crunch Fitness'),
+            ('American Barbell'),
+            ('UFC Fit');
     """,
         """
         DROP TABLE locations;
