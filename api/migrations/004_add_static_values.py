e917dc7ee07bb67efde548602fd50869495125ed
Corben M <corbenm99@gmail.com>
2023-08-30 11:38:47 -0700

    changed location table to only have city

---
+++ api/migrations/004_add_static_values.py
@@ -0,0 +1,13 @@
+steps = [
+    [
+        """
+            INSERT INTO locations (gym) VALUES
+            ('Planet Fitness'),
+            ('Sunset Gym'),
+            ('24hr fitness');
+    """,
+        """
+        DROP TABLE locations;
+""",
+    ],
+]
