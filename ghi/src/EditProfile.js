396a9a0615dc87e089566fea29f0d1b7e6679ad6
Corben Morrison <corbenm99@gmail.com>
2023-09-06 22:57:03 +0000

    Edit profile functionality #2 #29

--- ghi/src/EditProfile.js
+++ ghi/src/EditProfile.js
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useNavigate } from "react-router-dom";
 
 function EditProfile(props) {
   const {token, logout, fetchWithToken} = useToken();
-  const [username, setUsername] = useState(props.username);
-  const [firstName, setFirstName] = useState(props.firstName);
-  const [lastName, setLastName] = useState(props.lastName);
-  const [email, setEmail] = useState(props.email);
-  const [bio, setBio] = useState(props.bio);
-  const [avatar, setAvatar] = useState(props.avatar);
+  const [username, setUsername] = useState(props.user.username);
+  const [firstName, setFirstName] = useState(props.user.firstName);
+  const [lastName, setLastName] = useState(props.user.lastName);
+  const [email, setEmail] = useState(props.user.email);
+  const [bio, setBio] = useState(props.user.bio);
+  const [avatar, setAvatar] = useState(props.user.avatar);
+  const [password, setPassword] = useState("")
+  const [location, setLocation] = useState(props.user.location);
+
+  const [allLocations, setAllLocations] = useState([]);
+  const navigate = useNavigate();
+
+  async function getListLocations() {
+    const response = await fetch('http://localhost:8000/api/locations/');
+    if (response.ok){
+      const listLocations = await response.json();
+      setAllLocations(listLocations);
+    }
+  }
+
+  const logUserOut = async () => {
+      logout();
+      navigate("/login")
+    };
 
   const handleUsernameChange = (event) => {
     const value = event.target.value;
@@ -34,58 +53,88 @@ function EditProfile(props) {
     const value = event.target.value;
     setAvatar(value);
   }
+  const handlePasswordChange = (event) => {
+    const value = event.target.value;
+    setPassword(value);
+  }
+  const handleLocationChange = (event) => {
+    const value = event.target.value;
+    setLocation(value);
+  }
 
   async function handleSubmit(event) {
     event.preventDefault();
     const data = {};
-    data.id = props.id;
+    data.id = props.user.id;
     data.username = username;
+    data.email = email;
     data.firstName = firstName;
     data.lastName = lastName;
-    data.email = email;
     data.bio = bio;
     data.avatar = avatar;
+    data.password = password;
+    data.location_gym = location;
 
     const updateUrl = `http://localhost:8000/api/users/${data.id}`;
     const fetchConfig = {
       method: "PUT",
       body: JSON.stringify(data),
       headers: {
+             credentials: "include",
+             Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
         },
     };
-    const response = await fetchWithToken(updateUrl, fetchConfig);
+    const response = await fetch(updateUrl, fetchConfig);
     if (response.ok){
-      console.log("Cool & Epic")
+      logUserOut();
     }
   }
+
+  useEffect(() => {
+    getListLocations();
+  }, [])
 
     return (
   <form onSubmit={handleSubmit} id="edit-user-form">
   <div className="mx-auto border-success bg-light mb-3 mb-4 shadow p-3 mb-5 bg-body rounded " style={{width: "45rem"}} >
     <div className="mb-3">
-      <label htmlFor="email">Email address</label>
-      <input onChange={handleEmailChange} defaultValue="DefaultEmail@gmail.com" value={email} type="email" className="form-control" id="email" placeholder="name@example.com" />
+      <label htmlFor="Email">Email address</label>
+      <input onChange={handleEmailChange}  value={email} type="text" className="form-control" id="email" placeholder="name@example.com" />
     </div>
     <div className="mb-3">
       <label htmlFor="Username">Username</label>
       <input onChange={handleUsernameChange} value={username} type="text" className="form-control" id="username"/>
     </div>
+     <div className="mb-3">
+      <label htmlFor="Password">Password</label>
+      <input onChange={handlePasswordChange} value={password} type="text" className="form-control" id="password"/>
+    </div>
     <div className="mb-3">
-      <label htmlFor="Username">First name</label>
+      <label htmlFor="Firstname">First name</label>
       <input onChange={handleFirstNameChange} value={firstName} type="text" className="form-control" id="firstName" />
     </div>
     <div className="mb-3">
-      <label htmlFor="lastName">Last name</label>
+      <label htmlFor="Lastname">Last name</label>
       <input onChange={handleLastNameChange} value={lastName} type="text" className="form-control" id="lastName" />
     </div>
     <div className="mb-3">
-      <label htmlFor="bio" className="form-label">Bio</label>
+      <label htmlFor="Bio" className="form-label">Bio</label>
       <textarea onChange={handleBioChange} value={bio} className="form-control" id="bio" rows="3"></textarea>
     </div>
     <div className="mb-3">
-      <label htmlFor="avatar">Avatar</label>
+      <label htmlFor="Avatar">Avatar</label>
       <input type="text" onChange={handleAvatarChange} value={avatar} className="form-control" id="avatar" placeholder="ImageUrl"/>
+    </div>
+    <div className="mb-3">
+      <select id="Location" onChange={handleLocationChange} value={location} required name="location" className="form-select" >
+      <option value="">Choose a location</option>
+            {allLocations.map(location => {
+            return (
+            <option key={location.gym} value={location.gym}> {location.gym} </option>
+            );
+            })};
+      </select>
     </div>
     <button className="btn btn-primary">Finalize changes</button>
   </div>
--- ghi/src/ProfilePage.js
+++ ghi/src/ProfilePage.js
@@ -4,9 +4,9 @@ import { useNavigate } from "react-router-dom";
 import EditProfile from "./EditProfile";
 
 function UserProfilePage() {
-    // const params = useParams();
     const {token, logout, fetchWithToken} = useToken();
     const [user, setUser] = useState({});
+    const [location, setLocation] = useState({})
     const navigate = useNavigate();
     const [modalOpen, setModalOpen] = useState(false);
 
@@ -20,6 +20,7 @@ function UserProfilePage() {
       setModalOpen(!modalOpen);
     };
 
+
     const getUserData = async () => {
     if (token) {
       const url = `http://localhost:8000/api/accounts/`
@@ -30,6 +31,7 @@ function UserProfilePage() {
   useEffect(() => {
     getUserData();
   }, [token]);
+
 
     return (
       <div className="mx-auto border-success bg-light mb-3 mb-4 shadow p-3 mb-5 bg-body rounded " style={{width: "45rem"}} >
@@ -42,15 +44,15 @@ function UserProfilePage() {
           <li className="list-group-item border bg-light">Full Name: {user.firstName} {user.lastName}</li>
           <li className="list-group-item border bg-light">Email: {user.email}</li>
           <li className="list-group-item border bg-light">Bio: {user.bio}</li>
+          <li className="list-group-item border bg-light">Location: {user.location_gym}</li>
          </ul>
          </div>
         <div className="d-grid gap-2 d-md-flex justify-content-between">
           <button data-bs-toggle="modal" onClick={() => toggleModal()} className="btn btn-primary">Edit Profile</button>
           <button onClick={() => logUserOut()} className="btn btn-danger">Logout</button>
         </div>
-        {modalOpen && <EditProfile />}
+        {modalOpen && <EditProfile user={user} token={token} />}
       </div>
     );
-
 }
 export default UserProfilePage;
