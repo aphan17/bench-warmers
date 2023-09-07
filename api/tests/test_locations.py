11ae7546d167d2bb2ae1cbeee6f602e7e0058f1c
Corben M <corbenm99@gmail.com>
2023-09-07 10:30:44 -0700

    Updated favorites table to fix delete functionality for users

--- api/tests/test_locations.py
+++ api/tests/test_locations.py
@@ -1,7 +1,25 @@
 from fastapi.testclient import TestClient
 from main import app
-from queries import locationQueries
+from queries.locationQueries import LocationQueries
 
 client = TestClient(app)
 
-class
+
+class EmptyLocationQueries:
+    def get_all_locations(self):
+        return []
+
+
+def test_get_all_locations():
+    app.dependency_overrides[LocationQueries] = EmptyLocationQueries
+
+    response = client.get("/api/locations")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_init():
+    assert 1 == 1
