68ac27e58c9d2e3b65206b33512c42fcf5571f3b
Corben M <corbenm99@gmail.com>
2023-09-08 17:26:29 -0700

    Adding style to frontend, need more still also changed userRouter Exception

--- api/routers/usersRouters.py
+++ api/routers/usersRouters.py
@@ -82,7 +82,7 @@ async def create_account(
     hashed_password = authenticator.hash_password(info.password)
     try:
         account = repo.create_user(info, hashed_password)
-    except:
+    except Exception:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Cannot create an account with those credentials",
